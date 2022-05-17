ba37400fa19d70226ffc6beca57caa9aeb778b0e
SpandanaRao <vemulaspandana424@gmail.com>
2022-05-17 10:09:07 +0530

    arthmeticoperator

--- perimeter of rectangle/exercise-bt-operator-arithmeticoperator-master/exercise-bt-operator-arithmeticoperator-master/src/main/java/com/stackroute/basics/RectanglePerimeter.java
+++ perimeter of rectangle/exercise-bt-operator-arithmeticoperator-master/exercise-bt-operator-arithmeticoperator-master/src/main/java/com/stackroute/basics/RectanglePerimeter.java
@@ -1,6 +1,6 @@
 package com.stackroute.basics;
 
-
+import java.util.Scanner;
 
 public class RectanglePerimeter {
     public static void main(String[] args) {
@@ -9,11 +9,17 @@ public class RectanglePerimeter {
 
     //get user input from console
     public void getValues() {
-
+    	try (Scanner in = new Scanner(System.in)) {
+			double l=in.nextDouble();
+			double b=in.nextDouble();
+			double peri=perimeterCalculator(l,b);
+			System.out.println(peri);
+		}
     }
 
     //write logic to find perimeter of rectangle here
     public double perimeterCalculator(double length, double breadth) {
-        return 0;
+    	double per=2*(length+breadth);
+        return per;
     }
 }
